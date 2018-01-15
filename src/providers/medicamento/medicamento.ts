3a3639b7e894746cb0e2ea7ce6609410a24f5554
Diogo <diogo1z@hotmail.com>
2018-01-15 08:18:51 -0200

    mail

--- src/providers/medicamento/medicamento.ts
+++ src/providers/medicamento/medicamento.ts
@@ -69,8 +69,10 @@ export class MedicamentoProvider {
   public carregarDadosIniciais() {
     var medicamento22 = new Medicamento(); medicamento22.nome = "BOZORED"; medicamento22.principioAtivo = "BOZORED"; medicamento22.PF0 = 2766.23; medicamento22.PF12 = 3143.44; medicamento22.PF17 = 3332.81; medicamento22.PF17ALC = 3332.81; medicamento22.PF175 = 3353.00; medicamento22.PF175ALC = 3353.00; medicamento22.PF18 = 3373.45; medicamento22.PF18ALC = 3373.45; medicamento22.PF20 = 3457.79
     var medicamento23 = new Medicamento(); medicamento23.nome = "ACCORD FARMACÊUTICA LTDA"; medicamento23.principioAtivo = "BORTYZ"; medicamento23.PF0 = 2179.50; medicamento23.PF12 = 2476.71; medicamento23.PF17 = 2625.91; medicamento23.PF17ALC = 2625.91; medicamento23.PF175 = 2641.82; medicamento23.PF175ALC = 2641.82; medicamento23.PF18 = 2657.93; medicamento23.PF18ALC = 2657.93; medicamento23.PF20 = 2724.38
+    var medicamento99 = new Medicamento(); medicamento99.nome = "JANSSEN-CILAG FARMACÊUTICA LTDA"; medicamento99.principioAtivo = "VELCADE"; medicamento99.PF0 = 3353.01; medicamento99.PF12 = 3810.24; medicamento99.PF17 = 4039.78; medicamento99.PF175 = 4064.26; medicamento99.PF18 = 4089.04; medicamento99.PF20 = 4191.27;
     medicamento22.laboratoriosMedicamento = []; 
     medicamento22.laboratoriosMedicamento.push(medicamento23); 
+    medicamento22.laboratoriosMedicamento.push(medicamento99); 
     this.insert(medicamento22);
 
     var medicamento25 = new Medicamento(); medicamento25.nome = "DECITABINA"; medicamento25.principioAtivo = "DECITABINA"; medicamento25.PF0 = 2752.66; medicamento25.PF12 = 3128.02; medicamento25.PF17 = 3316.46; medicamento25.PF17ALC = 3316.46; medicamento25.PF175 = 3336.55; medicamento25.PF175ALC = 3336.55; medicamento25.PF18 = 3356.90; medicamento25.PF18ALC = 3356.90; medicamento25.PF20 = 3440.82
@@ -87,18 +89,20 @@ export class MedicamentoProvider {
     medicamento29.laboratoriosMedicamento.push(medicamento31); 
     this.insert(medicamento29);
 
-    var medicamento35 = new Medicamento(); medicamento35.nome = "CABAZRED"; medicamento35.principioAtivo = "CABAZRED"; medicamento35.PF0 = 13764.52; medicamento35.PF12 = 15641.50; medicamento35.PF17 = 16583.76; medicamento35.PF17ALC = 16583.76; medicamento35.PF175 = 16684.26; medicamento35.PF175ALC = 16684.26; medicamento35.PF18 = 16786.00; medicamento35.PF18ALC = 16786.00; medicamento35.PF20 = 17205.65
-    var medicamento36 = new Medicamento(); medicamento36.nome = "SANOFI-AVENTIS FARMACÊUTICA LTDA"; medicamento36.principioAtivo = "JEVTANA"; medicamento36.PF0 = 13764.97; medicamento36.PF12 = 15642.01; medicamento36.PF17 = 16584.31; medicamento36.PF17ALC = 16584.31; medicamento36.PF175 = 16684.81; medicamento36.PF175ALC = 16684.81; medicamento36.PF18 = 16786.55; medicamento36.PF18ALC = 16786.55; medicamento36.PF20 = 17206.21
-    var medicamento37 = new Medicamento(); medicamento37.nome = "EUROFARMA LABORATÓRIOS S.A."; medicamento37.principioAtivo = "PROAZITAX"; medicamento37.PF0 = 13764.97; medicamento37.PF12 = 15642.01; medicamento37.PF17 = 16584.31; medicamento37.PF17ALC = 16584.31; medicamento37.PF175 = 16684.81; medicamento37.PF175ALC = 16684.81; medicamento37.PF18 = 16786.55; medicamento37.PF18ALC = 16786.55; medicamento37.PF20 = 17206.21
+    var medicamento35 = new Medicamento(); medicamento35.nome = "CABAZRED"; medicamento35.principioAtivo = "CABAZRED"; medicamento35.PF0 = 13764.52; medicamento35.PF12 = 15641.50; medicamento35.PF17 = 16583.76; medicamento35.PF17ALC = 16583.76; medicamento35.PF175 = 16684.26; medicamento35.PF175ALC = 16684.26; medicamento35.PF18 = 16786.00; medicamento35.PF18ALC = 16786.00; medicamento35.PF20 = 17205.65;
+    var medicamento36 = new Medicamento(); medicamento36.nome = "SANOFI-AVENTIS FARMACÊUTICA LTDA"; medicamento36.principioAtivo = "JEVTANA"; medicamento36.PF0 = 13764.97; medicamento36.PF12 = 15642.01; medicamento36.PF17 = 16584.31; medicamento36.PF17ALC = 16584.31; medicamento36.PF175 = 16684.81; medicamento36.PF175ALC = 16684.81; medicamento36.PF18 = 16786.55; medicamento36.PF18ALC = 16786.55; medicamento36.PF20 = 17206.21;
+    var medicamento37 = new Medicamento(); medicamento37.nome = "EUROFARMA LABORATÓRIOS S.A."; medicamento37.principioAtivo = "PROAZITAX"; medicamento37.PF0 = 13764.97; medicamento37.PF12 = 15642.01; medicamento37.PF17 = 16584.31; medicamento37.PF17ALC = 16584.31; medicamento37.PF175 = 16684.81; medicamento37.PF175ALC = 16684.81; medicamento37.PF18 = 16786.55; medicamento37.PF18ALC = 16786.55; medicamento37.PF20 = 17206.21;
     medicamento35.laboratoriosMedicamento = []; 
     medicamento35.laboratoriosMedicamento.push(medicamento36); 
     medicamento35.laboratoriosMedicamento.push(medicamento37); 
     this.insert(medicamento35);
 
-    var medicamento40 = new Medicamento(); medicamento40.nome = "CABAZITAXEL"; medicamento40.principioAtivo = "CABAZITAXEL"; medicamento40.PF0 = 8947.02; medicamento40.PF12 = 10167.07; medicamento40.PF17 = 10779.54; medicamento40.PF17ALC = 10779.54; medicamento40.PF175 = 10844.87; medicamento40.PF175ALC = 10844.87; medicamento40.PF18 = 10911.00; medicamento40.PF18ALC = 10911.00; medicamento40.PF20 = 11183.78
-    var medicamento41 = new Medicamento(); medicamento41.nome = "EUROFARMA LABORATÓRIOS S.A."; medicamento41.principioAtivo = "CABAZITAXEL"; medicamento41.PF0 = 8947.23; medicamento41.PF12 = 10167.30; medicamento41.PF17 = 10779.79; medicamento41.PF17ALC = 10779.79; medicamento41.PF175 = 10845.12; medicamento41.PF175ALC = 10845.12; medicamento41.PF18 = 10911.25; medicamento41.PF18ALC = 10911.25; medicamento41.PF20 = 11184.03
+    var medicamento40 = new Medicamento(); medicamento40.nome = "CABAZITAXEL"; medicamento40.principioAtivo = "CABAZITAXEL"; medicamento40.PF0 = 8947.02; medicamento40.PF12 = 10167.07; medicamento40.PF17 = 10779.54; medicamento40.PF17ALC = 10779.54; medicamento40.PF175 = 10844.87; medicamento40.PF175ALC = 10844.87; medicamento40.PF18 = 10911.00; medicamento40.PF18ALC = 10911.00; medicamento40.PF20 = 11183.78;
+    var medicamento41 = new Medicamento(); medicamento41.nome = "EUROFARMA LABORATÓRIOS S.A."; medicamento41.principioAtivo = "CABAZITAXEL"; medicamento41.PF0 = 8947.23; medicamento41.PF12 = 10167.30; medicamento41.PF17 = 10779.79; medicamento41.PF17ALC = 10779.79; medicamento41.PF175 = 10845.12; medicamento41.PF175ALC = 10845.12; medicamento41.PF18 = 10911.25; medicamento41.PF18ALC = 10911.25; medicamento41.PF20 = 11184.03;
+    var medicamento98 = new Medicamento(); medicamento98.nome = "SANOFI-AVENTIS FARMACÊUTICA LTDA"; medicamento98.principioAtivo = "JEVTANA"; medicamento98.PF0 = 13764.97; medicamento98.PF12 = 15642.01; medicamento98.PF17 = 16584.31; medicamento98.PF17ALC = 16584.31; medicamento98.PF175 = 16684.81; medicamento98.PF175ALC = 16684.81; medicamento98.PF18 = 16786.55; medicamento98.PF18ALC = 16786.55; medicamento98.PF20 = 17206.21;
     medicamento40.laboratoriosMedicamento = []; 
     medicamento40.laboratoriosMedicamento.push(medicamento41); 
+    medicamento40.laboratoriosMedicamento.push(medicamento99); 
     this.insert(medicamento40);
 
     var medicamento27 = new Medicamento();medicamento27.nome = "REDTIBIN";medicamento27.principioAtivo = "REDTIBIN";medicamento27.PF0 = 4234.90;medicamento27.PF12 = 4812.38;medicamento27.PF17 = 5102.29;medicamento27.PF17ALC = 5102.29;medicamento27.PF175  = 5133.21;medicamento27.PF175ALC  = 5133.21;medicamento27.PF18  = 5164.51;medicamento27.PF18ALC  = 5164.51;medicamento27.PF20  = 5293.62;
@@ -107,7 +111,12 @@ export class MedicamentoProvider {
     medicamento27.laboratoriosMedicamento = []; 
     medicamento27.laboratoriosMedicamento.push(medicamento26); 
     this.insert(medicamento27);
-    
+
+    var medicamento95 = new Medicamento(); medicamento95.nome = "ABIRATERONA"; medicamento95.principioAtivo = "VELCADE"; medicamento95.PF0 = 8479.21; medicamento95.PF12 = 9798.47; medicamento95.PF17 = 10477.99; medicamento95.PF175 = 10551.17; medicamento95.PF18 = 10625.39; medicamento95.PF20 = 10933.02; 
+    var medicamento96 = new Medicamento(); medicamento96.nome = "JANSSEN-CILAG FARMACÊUTICA LTDA";medicamento95.principioAtivo = "VELCADE"; medicamento95.PF0 = 8479.21; medicamento95.PF12 = 9798.47; medicamento95.PF17 = 10477.99; medicamento95.PF175 = 10551.17; medicamento95.PF18 = 10625.39; medicamento95.PF20 = 10933.02; 
+    medicamento95.laboratoriosMedicamento = []; 
+    medicamento95.laboratoriosMedicamento.push(medicamento96); 
+    this.insert(medicamento95);
   }
 }
 
