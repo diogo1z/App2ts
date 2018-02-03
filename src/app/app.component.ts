cbfd9b4176e0959896a5180d091ed4dfc92dae69
Diogo <diogo1z@hotmail.com>
2018-02-03 12:45:16 -0200

    ajuste de tela branca

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,10 @@
-import { PropostaProvider, Proposta } from './../providers/proposta/proposta';
-import { UsuarioProvider } from './../providers/usuario/usuario';
-import { MedicamentoProvider, Medicamento } from './../providers/medicamento/medicamento';
-import { ClinicaProvider, Clinica } from './../providers/clinica/clinica';
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-import { LoadingController } from 'ionic-angular/components/loading/loading-controller';
-import { Http, Headers } from '@angular/http';
+
 
 @Component({
   templateUrl: 'app.html'
@@ -17,93 +12,19 @@ import { Http, Headers } from '@angular/http';
 export class MyApp {
   rootPage: any = null;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
-    medicamentoProvider: MedicamentoProvider, clinicaProvider: ClinicaProvider,
-    usuarioProvider: UsuarioProvider, public propostaProvider: PropostaProvider,
-    loading: LoadingController, public http: Http) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       splashScreen.hide();
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
-
-      
       this.openHomePage(splashScreen);
-      let loader = loading.create({
-        content: 'Carregando...',
-      });
-    
-      loader.present().then(() => {
-        medicamentoProvider.getAll()
-        .then((result: any[]) => {
-          if (result == null) {
-            clinicaProvider.clear();
-            clinicaProvider.carregarDadosIniciais();
-            medicamentoProvider.carregarDadosIniciais();
-            usuarioProvider.carregarDadosIniciais();
-          }
-          else if (result.length == 0) {
-            clinicaProvider.clear();
-            clinicaProvider.carregarDadosIniciais();
-            medicamentoProvider.carregarDadosIniciais();
-            usuarioProvider.carregarDadosIniciais();
-          }
-        })
-        .catch((e) => console.error(e));
-        console.log("1");
-        this.enviarPropostas();
-        console.log("2");
-        loader.dismiss();
-      });
     });
   }
 
   private openHomePage(splashScreen: SplashScreen) {
     splashScreen.hide();
     this.rootPage = HomePage;
-  }
-
-  private enviarPropostas() {
-    var erros = 0;
-
-    this.propostaProvider.getAllNaoEnviadas()
-      .then((result: any[]) => {
-        var link = 'http://www.drreddys.com.br/Email/api/Send';
-        var header = new Headers();
-        header.append('Content-Type', 'application/json;charset=utf-8');
-
-        for (var i = 0, len = result.length; i < len; i++) {
-          if (erros === 2)
-            break;
-          console.log(result);
-          console.log(i);
-          var proposta = result[i];
-          console.log(proposta);
-
-          var myData = JSON.stringify({
-            Destinatario: proposta.email,
-            CC: proposta.cc,
-            Assunto: "Proposta Dr. Reddy’s",
-            Corpo: proposta.mensagem,
-            Assinatura: ""
-          });
-          this.http.post(link, myData, { headers: header })
-            .subscribe(data => {
-              proposta.enviada = true;
-              proposta.enviarProposta = false;
-              this.propostaProvider.update(proposta);
-            }, error => {
-              erros++;
-              console.log("deu ruim");
-              console.log(proposta);
-              proposta.enviada = false;
-              proposta.enviarProposta = true;
-              this.propostaProvider.update(proposta);
-            });
-
-        };
-      })
-      .catch((e) => console.error(e));
   }
 }
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,23 +1,96 @@
 import { Component } from '@angular/core';
 import { NavController, ToastController } from 'ionic-angular';
 import { ContactProvider, Contact, ContactList } from '../../providers/contact/contact';
- 
+import { LoadingController } from 'ionic-angular/components/loading/loading-controller';
+import { Http, Headers } from '@angular/http';
+import { PropostaProvider, Proposta } from '../../providers/proposta/proposta';
+import { UsuarioProvider } from '../../providers/usuario/usuario';
+import { MedicamentoProvider, Medicamento } from '../../providers/medicamento/medicamento';
+import { ClinicaProvider, Clinica } from '../../providers/clinica/clinica';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
   contacts: ContactList[];
- 
-  constructor(public navCtrl: NavController, private contactProvider: ContactProvider, private toast: ToastController) { }
- 
-  ionViewDidEnter() {
-    this.contactProvider.getAll()
-      .then((result) => {
-        this.contacts = result;
-      });
+
+  constructor(public navCtrl: NavController, private contactProvider: ContactProvider,
+    private toast: ToastController, private medicamentoProvider: MedicamentoProvider, private clinicaProvider: ClinicaProvider,
+    private usuarioProvider: UsuarioProvider, public propostaProvider: PropostaProvider,
+    private loading: LoadingController, public http: Http) { }
+
+  ionViewDidLoad() {
+    let loader = this.loading.create({
+      content: 'Carregando...',
+    });
+
+    loader.present().then(() => {
+      this.medicamentoProvider.getAll()
+        .then((result: any[]) => {
+          if (result == null) {
+            this.clinicaProvider.clear();
+            this.clinicaProvider.carregarDadosIniciais();
+            this.medicamentoProvider.carregarDadosIniciais();
+            this.usuarioProvider.carregarDadosIniciais();
+          }
+          else if (result.length == 0) {
+            this.clinicaProvider.clear();
+            this.clinicaProvider.carregarDadosIniciais();
+            this.medicamentoProvider.carregarDadosIniciais();
+            this.usuarioProvider.carregarDadosIniciais();
+          }
+        })
+        .catch((e) => console.error(e));
+      this.enviarPropostas();
+
+      loader.dismiss();
+    });
   }
- 
+
+  private enviarPropostas() {
+    var erros = 0;
+
+    this.propostaProvider.getAllNaoEnviadas()
+      .then((result: any[]) => {
+        var link = 'http://www.drreddys.com.br/Email/api/Send';
+        var header = new Headers();
+        header.append('Content-Type', 'application/json;charset=utf-8');
+
+        for (var i = 0, len = result.length; i < len; i++) {
+          if (erros === 2)
+            break;
+          console.log(result);
+          console.log(i);
+          var proposta = result[i];
+          console.log(proposta);
+
+          var myData = JSON.stringify({
+            Destinatario: proposta.email,
+            CC: proposta.cc,
+            Assunto: "Proposta Dr. Reddy’s",
+            Corpo: proposta.mensagem,
+            Assinatura: ""
+          });
+          this.http.post(link, myData, { headers: header })
+            .subscribe(data => {
+              proposta.enviada = true;
+              proposta.enviarProposta = false;
+              this.propostaProvider.update(proposta);
+            }, error => {
+              erros++;
+              console.log("deu ruim");
+              console.log(proposta);
+              proposta.enviada = false;
+              proposta.enviarProposta = true;
+              this.propostaProvider.update(proposta);
+            });
+
+        };
+      })
+      .catch((e) => console.error(e));
+  }
+
   addContact() {
     this.navCtrl.push('EditContactPage');
   }
@@ -25,11 +98,11 @@ export class HomePage {
   irParaLogin() {
     this.navCtrl.push('LoginPage');
   }
- 
+
   editContact(item: ContactList) {
     this.navCtrl.push('EditContactPage', { key: item.key, contact: item.contact });
   }
- 
+
   removeContact(item: ContactList) {
     this.contactProvider.remove(item.key)
       .then(() => {
@@ -39,5 +112,4 @@ export class HomePage {
         this.toast.create({ message: 'Contato removido.', duration: 3000, position: 'botton' }).present();
       })
   }
- 
 }
